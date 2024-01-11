e5aef5ec3400e80bc43a323355c4521fd0e2c875
OZIIJIN <154957716+OZIIJIN@users.noreply.github.com>
2024-01-11 18:04:45 +0900

    createStudent 구현

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,14 +1,26 @@
 package model;
 
+import javax.naming.ServiceUnavailableException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String studentId;
     private String studentName;
-
-    public Student(String seq, String studentName) {
+    private List<Subject> selectedmandatoryList;
+    private List<Subject> selectedchoiceList;
+    public void setStudentId(String seq){
         this.studentId = seq;
+    }
+    public void setStudentName(String studentName){
         this.studentName = studentName;
     }
-
+    public void setSelectedmandatoryList(List<Subject> subjects){
+        this.selectedmandatoryList = subjects;
+    }
+    public void setSelectedchoiceList(List<Subject> subjects){
+        this.selectedchoiceList = subjects;
+    }
     // Getter
     public String getStudentId() {
         return studentId;
@@ -16,6 +28,12 @@ public class Student {
 
     public String getStudentName() {
         return studentName;
+    }
+    public List<Subject> getSelectedmandatoryList(){
+        return selectedmandatoryList;
+    }
+    public List<Subject> getSelectedchoiceList(){
+        return selectedchoiceList;
     }
 
 }
